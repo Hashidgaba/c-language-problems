52dcd4276a86918f54d86b6f14f8dcff19f7bb44
Hashidgaba <hashidgaba@gmail.com>
2025-02-11 15:06:25 +0500

    done sum of the series

---
+++ sum of the series/code.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout << "Enter any Number" << endl;
+    cin >> num;
+
+    int sum = 0;
+    int i = 1;
+    while(i <= num){
+        cout << i << " * " <<  i << " = " << i * i << endl;
+        sum += i * i;
+        i++;
+    }
+
+    cout << "The sum of the above series is: " << sum << endl;
+    return 0;
+}
