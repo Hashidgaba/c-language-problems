1ed6fa6358b04c2d6bd29ec7296cc9690fa8c25c
Hashidgaba <hashidgaba@gmail.com>
2025-02-18 23:04:54 +0500

    done char array

--- char array/task.cpp
+++ char array/task.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 using namespace std;
+void changeCses(char arr[]){
+    for(int i = 0; arr[i] != '\0';i++){
+        if(arr[i] >= 'a' && arr[i] <= 'z'){
+            arr[i] = arr[i] -32;
+        }else if(arr[i] >= 'A' && arr[i] <= 'Z'){
+            arr[i] = arr[i] + 32;
+        }
+    }
 
+    cout << "your name is : " << arr << endl;
+}
 int main(){
     char userName[100];
     cout << "Enter your Name" << endl;
     cin.getline(userName, 100);
-    for(int i = 0; userName[i] != '\0'; i++){
-        if(userName[i] >= 'a' && userName[i] <= 'z'){
-            userName[i] = userName[i] - 32;
-        }else if(userName[i] >= 'A' && userName[i] <= 'Z'){
-            userName[i] = userName[i] + 32;
-        }
-    }
-    cout << "your name is: " << userName;
+    changeCses(userName);
+    
     return 0;
 }
