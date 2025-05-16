c3863d2ab64c2afb624e810877400b3642be8eb0
Hashidgaba <hashidgaba@gmail.com>
2025-05-16 20:30:34 +0500

    done updates

--- char array/task.cpp
+++ char array/task.cpp
@@ -1,21 +1,42 @@
 #include <iostream>
 using namespace std;
-void changeCses(char arr[]){
-    for(int i = 0; arr[i] != '\0';i++){
-        if(arr[i] >= 'a' && arr[i] <= 'z'){
-            arr[i] = arr[i] -32;
-        }else if(arr[i] >= 'A' && arr[i] <= 'Z'){
+int fac(int n){
+    int product = 1;
+    for(int i = n; i >=1; i--){
+        product *=i;
+    }
+    return product;
+}
+void changeCses(char arr[])
+{
+   
+    int length = 0;
+    for (int i = 0; arr[i] != '\0'; i++)
+    {
+        length++;
+    }
+
+    for (int i = 0; arr[i] != '\0'; i++)
+    {
+        if (arr[i] >= 'a' && arr[i] <= 'z')
+        {
+            arr[i] = arr[i] - 32;
+        }
+        else if (arr[i] >= 'A' && arr[i] <= 'Z')
+        {
             arr[i] = arr[i] + 32;
         }
     }
 
     cout << "your name is : " << arr << endl;
+
+    cout << "The factorial of the length of your name is " << fac(length);
 }
-int main(){
-    char userName[100];
-    cout << "Enter your Name" << endl;
-    cin.getline(userName, 100);
-    changeCses(userName);
-    
+int main()
+{
+    char user[100];
+    cout << "Enter your name" << endl;
+    cin.getline(user, 100);
+    changeCses(user);
     return 0;
 }
